627c8c3b0fa5e9ef648536839b63219285e62ed9
Dyl6875 <dydylou24@gmail.com>
2024-11-26 17:06:13 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,21 +1,54 @@
-let cle = Object.keys(SAE)
-let description = []
-let compétences = []
-let sae = ""
+let sae = Object.keys(SAE); // Récupère les clés des SAE
+console.log(sae);
 
-for (let index = 0; index < cle.length; index++) {
+let titresae = ''; // Génère la liste des SAE
 
-    description.push(SAE[cle[index]].titre)
-    compétences.push(SAE[cle[index]].compétences)
+let titre = Object.values(SAE).map(sae => sae.titre); // Titres des SAE
+console.log(titre);
 
+let compétences = Object.values(SAE).map(sae => sae.compétences); // Compétences des SAE
+
+// Génère la liste des SAE (pour la page 2)
+for (let i = 0; i < sae.length; i++) {
+    titresae += `
+        <a href='/SAE.html?sae=${sae[i]}'> 
+            <div class='sae'> 
+                <h1 class='centre'>${sae[i]}</h1> 
+                <p>${titre[i]}</p> 
+                <p class='compétence'> ${compétences[i].join(', ')}</p>
+            </div>
+        </a>
+    `;
 }
 
-for (let forme = 0; forme < cle.length; forme++) {
-
-    sae += '<a href="sae.html"><div class="sae"><h2>' + cle[forme] + '</h2><div class="block">' + description[forme]
-    + '</div><div class="block">' + compétences[forme] + '</div></div></a>'
-
+if (document.querySelector(".listee")) {
+    document.querySelector(".listee").innerHTML = titresae;
 }
 
-document.querySelector(".liste").innerHTML = sae
 
+document.querySelector(".liste").innerHTML = sae;
+
+const params = new URLSearchParams(windows.location.search);
+
+const saeID = params.get('sae');
+
+if (saeID && SAE[saeID]) {
+    const saeData = SAE[saeID];
+    let content = `
+    <div class="sae-contenue">
+        <p><strong class='description'>Description :</strong> ${saeData.description}</p>
+        <h3>Activités</h3>
+        <ul>
+            <li>${saeData.AC ? Object.values(saeData.AC).join('</li><li>') : 'Aucune activité'}</li>
+        </ul>
+        <h3>Ressources</h3>
+        <ul>
+            <li>${saeData.ressources ? Object.values(saeData.ressources).join('</li><li>') : 'Aucune ressource'}</li>
+        </ul>
+        <p><strong>Semestre :</strong> ${saeData.semestre}</p>
+         </div>
+    `;
+
+    document.getElementById('app').innerHTML = content;
+
+} 
