ad45fe5c73f6f9a8559fdcaf2216f6204c605655
Dyl6875 <dydylou24@gmail.com>
2024-11-28 09:51:38 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-let sae = Object.keys(SAE); 
+let sae = Object.keys(SAE);
 console.log(sae);
 
-let titresae = ''; 
-let titre = Object.values(SAE).map(sae => sae.titre); 
+let titresae = '<div class="flex2">';
+let titre = Object.values(SAE).map(sae => sae.titre);
 console.log(titre);
 
-let compétences = Object.values(SAE).map(sae => sae.compétences); 
+let compétences = Object.values(SAE).map(sae => sae.compétences);
 
 for (let i = 0; i < sae.length; i++) {
     titresae += `
+    <div>
         <a href='/sae.html?sae=${sae[i]}'> 
-            <div class='S'> 
-                <h1 class='titel'>${sae[i]}</h1> 
+            <div class='sae'> 
+                <h1 class='centre'>${sae[i]}</h1> 
                 <p>${titre[i]}</p> 
-                <p class='compétence'> ${compétences[i].join(', ')}</p>
+                <p class='bout boutt'> ${compétences[i].join(', ')}</p>
             </div>
         </a>
+    </div>
     `;
 }
 
@@ -24,32 +26,33 @@ if (document.querySelector(".listee")) {
 }
 
 const params = new URLSearchParams(window.location.search);
-const saeID = params.get('sae'); 
+const saeID = params.get('sae');
+
+
 
 if (saeID && SAE[saeID]) {
-    const saeData = SAE[saeID]; 
+    const saeData = SAE[saeID];
 
     let content = `
-    <div class="saecontenue">
-        <p><strong class='description'>Description :</strong> ${saeData.description}</p>
-        <h3>Activités</h3>
+    <div class="cent">
+        <p><strong class='block centre'>Description :</strong> ${saeData.description}</p>
+        <h3 class="cent2">Activités :</h3>
         <ul>
-            <li>${saeData.AC ? Object.values(saeData.AC).join('</li><li>') : 'Aucune activité'}</li>
+            - ${saeData.AC ? Object.values(saeData.AC).join('<br> - ') : 'Aucune activité'}
         </ul>
-        <h3>Ressources</h3>
+        <h3 class="cent2">Ressources :</h3>
         <ul>
-            <li>${saeData.ressources ? Object.values(saeData.ressources).join('</li><li>') : 'Aucune ressource'}</li>
+            - ${saeData.ressources ? Object.values(saeData.ressources).join('<br> - ') : 'Aucune ressource'}
         </ul>
-        <p><strong>Semestre :</strong> ${saeData.semestre}</p>
+        <p><strong class="cent2">Semestre :</strong> ${saeData.semestre}</p>
          </div>
     `;
 
     document.getElementById('app').innerHTML = content;
 
-} 
+}
 
-else if (document.getElementById('app')) 
-    {
+else if (document.getElementById('app')) {
 
-    document.getElementById('app').innerHTML = '<p>SAE non trouvée !</p>';
+    document.getElementById('app').innerHTML = '<p class="centre" >La page est introuvable...</p>';
 }
